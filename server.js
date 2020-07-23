97d1ddb79e8d2747d091705f2cc0d5f16347dd6c
HASSAN AMJAD <hassan004z@hotmail.com>
2020-07-23 23:34:34 +0500

    perform rendering

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const express = require ('express')
 const app = express()
 const ReactDOMServer = require ("react-dom/server");
 const App = require('./dist/index')
+
+app.use(express.static('dist'))
+
+app.get('/' , (req,res) => {
+    res.send()
+})
+
